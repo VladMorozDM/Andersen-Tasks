b25a2adc846025f810eeccbc599b79810ef4cd09
Moroz Vladislav <nikeblack97@gmail.com>
2019-04-23 17:54:07 +0300

    Added a few features:  1) User can delete an Item;  2) User can add a new Item;  3) Class-Commander now have method to sort items by time (by text in progress), but it not binded to select option  3) Added static select menu.
     Also I fixed bug with items added via form, now you can delete them.

--- index.js
+++ index.js
@@ -5,24 +5,25 @@ const parent = document.getElementById("root");
 const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
 ];
+
 const itemsToParse  = [
     {
-        date: new Date( '2019-04-23T03:24:00' ),
+        date:  '2019-04-23T03:24:00' ,
         text: 'a',
         id: 1
     },
     {
-        date: new Date( '2019-04-23T03:24:00' ),
+        date:  '2010-04-23T03:24:00' ,
         text: 'b',
         id: 2
     },
     {
-        date: new Date( '2019-04-23T03:24:00' ),
+        date:  '2019-02-23T03:24:00' ,
         text: 'c',
         id: 3
     },
     {
-        date: new Date( '2019-04-23T03:24:00' ),
+        date: '2019-05-23T03:24:00' ,
         text: 'd',
         id: 4
     }
@@ -34,53 +35,100 @@ class Commander{
     constructor(root, toDoItems = []){
         this.toDoItems = toDoItems;
         this.root = root;
-
+        this.state = "date"
     }
 
     add(item){
         const toDoItems = this.toDoItems;
         if(!item.id)  (item.id = toDoItems[toDoItems.length-1].id+1);
         this.toDoItems.push(item);
-        this.render()
+        this.render(this.toDoItems)
     };
 
     remove(itemId){
         this.toDoItems = this.toDoItems.filter( item => item.id !== itemId);
-
-        this.render();
+        this.render(this.toDoItems);
     }
+
+    getTemplate(){
+        return `<div>
+                    <form id="addItem" action="#">
+                        <div>
+                            <input type="text">
+                            <input type="button" onclick="addItems()" value="add Task">
+                        </div>
+                        <select name="language" size="2">
+                            <option value="byTime"="selected">Sort By Time</option>
+                            <option value="byText">Sort By Text</option>
+                        </select>
+                    </form>
+                    
+                </div>`
+    }
+
+    filterItems(criteria){
+        /*
+        switch(criteria) {
+            case "byTime":
+
+                break;
+            case "byText":
+
+                break;
+        }
+        */
+        };
+    sortItems(criteria){
+        switch(criteria) {
+            case "byTime":
+                const sortedList = this.toDoItems.sort(function(a,b){
+                    const c = new Date(a.date);
+                    const d = new Date(b.date);
+                    return c-d;
+                });
+                console.log(sortedList);
+                this.render(sortedList);
+                break;
+            case "byText":
+
+                break;
+        }
+
+    };
 
 
     onInit(){
-        this.render();
-
-
-    }
-
-    render(){
-        this.root.innerHTML = '';
-        this.toDoItems.forEach( item => {
-            const wrapper = document.createElement("div");
-            wrapper.innerHTML = item.template;
-            this.root.append(wrapper);
-        });
+        this.render(this.toDoItems);
         this.root.addEventListener("click", e =>{
             this.toDoItems.forEach( item => {
                 item.handleClick(this, e)} )
         });
+    }
+
+    render(Items){
+        this.root.innerHTML = '';
+
+        Items.forEach( item => {
+            const wrapper = document.createElement("div");
+            wrapper.innerHTML = item.getTemplate();
+            this.root.prepend(wrapper);
+        });
+        const form = this.getTemplate();
+        this.root.innerHTML +=  form;
     }
 }
 
 
 
 class Item{
-
     constructor({date = '', text = '', id = 0}){
         this.date = date;
         this.text = text;
         this.id = id;
-        console.log(this.date, "&");
-        this.template = `<h6> ${monthNames[this.date.getMonth()]} ${this.date.getDay()} </h6>
+
+    }
+    getTemplate(){
+        return `<h6> ${new Date(this.date)} </h6>
             <form action="">
                 <input type="checkbox">
                 <p>${this.text}</p>                    
@@ -90,50 +138,10 @@ class Item{
                 <input type="button" name = "submit"><input type="button" name = "cancel">
              </form>`;
     }
-
     handleClick(controller, event){
         if(event.target.getAttribute("data-id") == this.id  ) {
             if (event.target.classList.contains("delete")) {
-                console.log("IN!")
                 controller.remove(this.id);
-            }
-            if(event.target.classList.contains("submit")){
-                event.target.setAttribute("value", "edit");
-                event.target.classList.toggle("edit");
-                event.target.classList.toggle("submit");
-                let contentToChange = event.target;
-                while(contentToChange.id != this.id){
-                    contentToChange = contentToChange.previousElementSibling;
-                }
-
-                if(this.name !== contentToChange.firstElementChild.value){
-                    this.name = contentToChange.firstElementChild.value
-                    this.template = `<p id="${this.id}">${this.name}</p>
-                          <input type="button" class = "edit" data-id="${this.id}" value="edit">
-                          <input type="button" class = "delete" data-id="${this.id}" value="delete">`;
-                    contentToChange.innerHTML = contentToChange.firstElementChild.value
-                }else{
-
-                    contentToChange.innerHTML = contentToChange.firstElementChild.value;
-
-
-                }
-            }
-
-
-            // Click on edit button
-            else if(event.target.classList.contains("edit")){
-                event.target.setAttribute("value", "submit");
-                event.target.classList.toggle("edit");
-                event.target.classList.toggle("submit");
-                let contentToChange = event.target;
-                while(contentToChange.id != this.id){
-                    contentToChange = contentToChange.previousElementSibling;
-                }
-
-                const contentValue = contentToChange.innerHTML;
-                contentToChange.innerHTML =
-                    `<input type="text" value="${contentValue}">`
             }
 
         }
@@ -141,17 +149,17 @@ class Item{
 }
 
 
-const taskList = itemsToParse.map( item => new Item(item));
-
-const firstComm = new Commander(parent, taskList);
-firstComm.onInit();
-
-const form = document.getElementById("addItem");
-
-
 function addItems(event){
-    console.log("Ku!");
-
+    const form = document.getElementById("addItem");
     const value = new Item({date: new Date(),text: form.elements[0].value});
     firstComm.add(value);
 }
+
+const taskList = itemsToParse.map( item => new Item(item));
+const firstComm = new Commander(parent, taskList);
+firstComm.onInit();
+
+
+firstComm.sortItems("byTime");
+
+
